cfd87a3d0365388bfc9c826f4cb5ad32acc42fae
Elena <elenazhilina@yandex.ru>
2024-05-19 08:29:56 +0500

    Upgrade CDP

--- src/test/java/ru/netology/WebTest.java
+++ src/test/java/ru/netology/WebTest.java
@@ -13,11 +13,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class WebTest {
     private WebDriver driver;
 
-    @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\YZhilina\\Documents\\Documents\\Lena\\OWN\\Netology\\Chromedriver\\chromedriver-win64\\chromedriver.exe");
-        WebDriverManager.chromedriver().setup();
-    }
+    //   @BeforeAll
+//    static void setUpAll() {
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\YZhilina\\Documents\\Documents\\Lena\\OWN\\Netology\\Chromedriver\\chromedriver-win64\\chromedriver.exe");
+//        WebDriverManager.chromedriver().setup();
+//    }
 
 
     @BeforeEach
@@ -44,10 +44,10 @@ class WebTest {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+77772100609");
         driver.findElement(By.cssSelector("[data-test-id=agreement] input")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id=order-success] input")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success].input")).getText();
         assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
 
-        Thread.sleep(5000);
+        Thread.sleep(10000);
     }
 }
 
